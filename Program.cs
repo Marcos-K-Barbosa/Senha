6af367eb161315bdc6b7ac33148f609fcab58c3a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 20:42:57 -0300

    Programa Concluído com Exatidão.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿String SenhaDigitada, SenhaCorreta;
+ 
+SenhaCorreta = "Bombardilo Crocodilo";
+
+Console.Clear();
+Console.Write(@"Seja bem vindo, caro Web Navegador! É sempre um prazer te ver por aqui. 
+Mas antes de tudo é necessário que você confirme a seu login para sabermos que realmente é você, caro navegador.
+Digite sua senha: ");
+SenhaDigitada = Console.ReadLine()!;
+
+if(SenhaDigitada==SenhaCorreta)
+
+{
+//Alternativa Correta.
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Acesso Concedido.");
+}
+
+
+else
+
+{
+//Alternativa Incorreta.
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Acesso Negado.");
+}
+
+Console.ResetColor();
